dfe3a74c17ada2c46b6b13c08423ffb64660e730
guo <guo@mac.com>
2015-04-24 00:42:47 +0800

    理解了全局变量

--- explore/views.py
+++ explore/views.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from django.http import HttpResponse
+class Student(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        return self.name +str(self.score)
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+
+theStudent =Student('Bart Simpson', 59)
 
 # Create your views here.
 def index(request):
-    return render(request, 'explore/index.html')
+    #return render(request, 'explore/index.html')
+    return HttpResponse(theStudent.name)
---
+++ func/filldb.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+#from main.models import userinfo ,photo
+import datetime
+from django.utils import timezone
+import os, sys
+import random ,codecs
+
+photoPath =r"/Users/guopeng/Documents/panoramio/photos"
+resultpath =r"/Users/guopeng/Documents/panoramio/15"
+
+def createphoto():
+    dList =[]
+    theDict =dict()
+    with codecs.open(resultpath, 'r', 'utf-8') as f:
+        tList =[line.strip('\n') for line in f.readlines()]
+    resultList =[x.split('||') for x in tList]
+    for x in resultList:
+        if len(x) <15:
+            dList.append(x)
+    for x in dList:
+        resultList.remove(x)
+    for x in resultList:
+        for idx in range(-4,0):
+            if '|' in x[idx]:
+                x[idx] =x[idx].strip('|')
+            if x[idx] =='':
+                x[idx] ='0'
+            x[idx] =int(x[idx])
+
+    for x in resultList:
+        theDict[x[0]] =x
+
+    #写数据库
+    theUser = User.objects.get(pk=1)
+    for x in resultList:
+        thePhoto =photo()
+        thePhoto.testid =x[0]
+        thePhoto.user =theUser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#>>> from thetest.createqc import createqcs
+#>>> createqcs()
